9a1541cc9483bccd14fdebbdf032b68b48e98e4b
Salman Altuwayjiri <alk_333@hotmail.com>
2019-04-16 11:24:19 +0300

    created game ligic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -7,22 +7,30 @@ var cardFour = "king";
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardFour);
 */
+
+
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
 
-console.log("User flipped " + cardsInPlay[0]);
+var checkForMatch = function(){
 
-var cardTwo = cards[2];
+  if (cardsInPlay[0] === cardsInPlay[1]) 
+    console.log("You found a match!");
+  else
+    console.log("Sorry, try again.");
 
-cardsInPlay.push(cardTwo);
+  }
 
-console.log("User flipped " + cardsInPlay[1]);
 
-if(cardsInPlay.length === 2){
- if(cardsInPlay[0] === cardsInPlay[1])
-     alert("You found a match!");
- else
-  alert("Sorry, try again.")
+
+var flipCard = function(cardId){
+  console.log("User flipped " + cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
+
+  if(cardsInPlay.length === 2)
+  checkForMatch();
+
 }
+
+flipCard(1);
+flipCard(3);
